190d16d2f258c6c05382c4f3e3c2fdf80aa13b9c
Jaden Ong <ong.jadengabriel@gmail.com>
2025-12-14 11:36:23 -0800

    update /me endpoint for clarification

--- routers/auth.py
+++ routers/auth.py
@@ -86,5 +86,5 @@ def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
 
 @router.get("/me", response_model=UserOut)
 def get_current_user_info(current_user: User = Depends(get_current_user)):
-    """Get current user information."""
+    """Get current authenticated user information."""
     return current_user
