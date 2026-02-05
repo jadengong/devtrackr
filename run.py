fc35e7b64dd9a1a98e14064e34697b3b519cf52d
Jaden Ong <ong.jadengabriel@gmail.com>
2026-02-04 22:51:20 -0800

    Make run.py host/port configurable via env vars

--- run.py
+++ run.py
@@ -11,5 +11,9 @@ from src.main import app
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+    host = os.getenv("DEVTRACKR_HOST", "127.0.0.1")
+    port = int(os.getenv("DEVTRACKR_PORT", "8000"))
+
+    uvicorn.run("src.main:app", host=host, port=port, reload=True)
